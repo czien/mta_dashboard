36de634828892c388d82d426df5b7e192bf097be
Charlie <charlie.zien@gmail.com>
2019-08-04 23:37:52 -0400

    Formatting cleanup

--- mta_dashboard_app/server.R
+++ mta_dashboard_app/server.R
@@ -1,4 +1,6 @@
 
+
+
 shinyServer(function(input, output, session) {
   observe({
     updatePickerInput(session, "line",
@@ -19,9 +21,9 @@ shinyServer(function(input, output, session) {
       summarise(`additional platform time` = mean(`additional platform time`)) %>%
       mutate(period = period)
     g = ggplot(df,
-           aes(Date,
-               `additional platform time`,
-               color = period)) +
+               aes(Date,
+                   `additional platform time`,
+                   color = period)) +
       theme +
       scale_color_brewer(palette = theme_palette) +
       geom_point() +
@@ -29,9 +31,12 @@ shinyServer(function(input, output, session) {
       scale_x_datetime(date_breaks = "months", labels = date_format("%b %Y")) +
       labs(x = "Date", y = "Extra Platform Time (minutes)")
     
-    ggplotly(g) %>% 
-      layout(legend = list(orientation = "h",
-                           y = -.5, x = 0.4)) 
+    ggplotly(g) %>%
+      layout(legend = list(
+        orientation = "h",
+        y = -.5,
+        x = 0.4
+      ))
   })
   
   output$train_time = renderPlotly({
@@ -42,9 +47,9 @@ shinyServer(function(input, output, session) {
       summarise(`additional train time` = mean(`additional train time`)) %>%
       mutate(period = period)
     g = ggplot(df,
-           aes(Date,
-               `additional train time`,
-               color = period)) +
+               aes(Date,
+                   `additional train time`,
+                   color = period)) +
       theme +
       scale_color_brewer(palette = theme_palette) +
       geom_point() +
@@ -52,11 +57,14 @@ shinyServer(function(input, output, session) {
       scale_x_datetime(date_breaks = "months", labels = date_format("%b %Y")) +
       labs(x = "Date", y = "Average Extra Train Time (Minutes)")
     
-    ggplotly(g) %>% 
-      layout(legend = list(orientation = "h",
-                           y = -.5, x = 0.4)) 
+    ggplotly(g) %>%
+      layout(legend = list(
+        orientation = "h",
+        y = -.5,
+        x = 0.4
+      ))
   })
-
+  
   output$on_time_performance = renderPlotly({
     df = on_time_performance %>% filter(line %in% input$line &
                                           Date >= input$date_range[1] &
@@ -65,21 +73,22 @@ shinyServer(function(input, output, session) {
       summarise(on_time_performance = mean(on_time_performance)) %>%
       mutate(period = period)
     g = ggplot(df,
-           aes(
-             Date,
-             on_time_performance,
-             color = period
-           )) +
+               aes(Date,
+                   on_time_performance,
+                   color = period)) +
       theme +
       scale_color_brewer(palette = theme_palette) +
       geom_point() +
       geom_line() +
       scale_x_datetime(date_breaks = "months", labels = date_format("%b %Y")) +
-      labs(x="Date", y="On-Time Performance Ratio")
+      labs(x = "Date", y = "On-Time Performance Ratio")
     
-    ggplotly(g) %>% 
-      layout(legend = list(orientation = "h",
-                           y = -.5, x = 0.4)) 
+    ggplotly(g) %>%
+      layout(legend = list(
+        orientation = "h",
+        y = -.5,
+        x = 0.4
+      ))
   })
   
   output$el_es = renderPlotly({
@@ -93,68 +102,77 @@ shinyServer(function(input, output, session) {
       geom_point() +
       geom_line() +
       scale_x_datetime(date_breaks = "months", labels = date_format("%b %Y")) +
-      labs(x="Date", y="% Functional")
+      labs(x = "Date", y = "% Functional")
     
-    ggplotly(g) %>% 
-      layout(legend = list(orientation = "h",
-                           y = -.5, x = 0.3)) 
+    ggplotly(g) %>%
+      layout(legend = list(
+        orientation = "h",
+        y = -.5,
+        x = 0.3
+      ))
   })
   
   output$incidents = renderPlotly({
     df = incidents %>% filter(line %in% input$line &
                                 Date >= input$date_range[1] &
                                 Date <= input$date_range[2]) %>%
-      mutate(category = as.factor(category)) %>% 
+      mutate(category = as.factor(category)) %>%
       group_by(Date, category) %>%
       summarise(count = sum(count))
-    g = (ggplot(df,
-           aes(
-             Date,
-             count,
-             fill = category
-           )) +
-      theme +
-      scale_fill_brewer(palette = theme_palette) +
-      geom_area(position = "stack", stat = "identity") +
-      scale_x_datetime(date_breaks = "months", labels = date_format("%b %Y"))) +
-      labs(x="Date", y="Count")
+    g = (
+      ggplot(df,
+             aes(Date,
+                 count,
+                 fill = category)) +
+        theme +
+        scale_fill_brewer(palette = theme_palette) +
+        geom_area(position = "stack", stat = "identity") +
+        scale_x_datetime(date_breaks = "months", labels = date_format("%b %Y"))
+    ) +
+      labs(x = "Date", y = "Count")
     
-    ggplotly(g) %>% 
-      layout(legend = list(orientation = "h",
-                          y = -.5, x = 1)) 
+    ggplotly(g) %>%
+      layout(legend = list(
+        orientation = "h",
+        y = -.5,
+        x = 1
+      ))
   })
   
   
   output$serv_del = renderPlotly({
     df = serv_del[, -6] %>%
-      mutate(Delta = num_sched_trains - num_actual_trains,
-             `Trains Run` = num_actual_trains,
-              Line = line) %>%
+      mutate(
+        Delta = num_sched_trains - num_actual_trains,
+        `Trains Run` = num_actual_trains,
+        Line = line
+      ) %>%
       filter(Line %in% input$line & Date >= input$date_range[1] &
-               Date <= input$date_range[2]) %>% 
-      gather("Metric", "Value", 7,8) %>%
+               Date <= input$date_range[2]) %>%
+      gather("Metric", "Value", 7, 8) %>%
       group_by(Date, Metric) %>%
       summarise(Value = sum(Value))
-      
     
-      trains_vec = df$Value[df$Metric == "Trains Run"]
+    
+    trains_vec = df$Value[df$Metric == "Trains Run"]
     difs_vec = df$Value[df$Metric == "Delta"]
     g = ggplot(df,
-           aes(
-             Date,
-             Value,
-             fill = Metric
-           )) +
+               aes(Date,
+                   Value,
+                   fill = Metric)) +
       theme +
       scale_fill_brewer(palette = theme_palette) +
       scale_x_datetime(date_breaks = "months", labels = date_format("%b %Y")) +
       geom_area() +
       coord_cartesian(ylim = c(min(trains_vec) * .85, max(trains_vec + difs_vec) *
                                  1.1)) +
-      labs(x="Date", y="Trains Run vs. Scheduled")
-    ggplotly(g) %>% 
-      layout(legend = list(orientation = "h",
-                           y = -.5, x = 0.3)) 
+      labs(x = "Date", y = "Trains Run vs. Scheduled")
+    ggplotly(g) %>%
+      layout(legend = list(
+        orientation = "h",
+        y = -.5,
+        x = 0.3
+      ))
   })
   
 })
--- mta_dashboard_app/ui.R
+++ mta_dashboard_app/ui.R
@@ -1,38 +1,40 @@
 
 
+
 shinyUI(fluidPage(
-    titlePanel("MTA Performance Data"),
-    sidebarLayout(
-        sidebarPanel(
-            pickerInput(
-                "line",
-                "Line:",
-                choices = lines,
-                options = list(`actions-box` = TRUE),
-                multiple = T,
-                selected = lines
-            ),
-            dateRangeInput(
-                "date_range",
-                "Date Range:",
-                start = "2018-06-01",
-                end = "2019-06-01",
-                min = "2015-01-01",
-                format = "mm/yy"
-            ),
-            width = 2
-            
-        ),
-        
-        mainPanel(
-            tabsetPanel(
-                tabPanel("On-Time Performance", plotlyOutput("on_time_performance")),
-                tabPanel("Additional Platform Time", plotlyOutput("plat_time")),
-                tabPanel("Additional Train Time", plotlyOutput("train_time")),
-                tabPanel("Incidents", plotlyOutput("incidents")),
-                tabPanel("Service Delivery", plotlyOutput("serv_del")),
-                tabPanel("Elevators/Escalators", plotlyOutput("el_es"))
-            )
-            
+  titlePanel("MTA Performance Data"),
+  sidebarLayout(
+    sidebarPanel(
+      pickerInput(
+        "line",
+        "Line:",
+        choices = lines,
+        options = list(`actions-box` = TRUE),
+        multiple = T,
+        selected = lines
+      ),
+      dateRangeInput(
+        "date_range",
+        "Date Range:",
+        start = "2018-06-01",
+        end = "2019-06-01",
+        min = "2015-01-01",
+        format = "mm/yy"
+      ),
+      width = 2
+      
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("On-Time Performance", plotlyOutput("on_time_performance")),
+        tabPanel("Additional Platform Time", plotlyOutput("plat_time")),
+        tabPanel("Additional Train Time", plotlyOutput("train_time")),
+        tabPanel("Incidents", plotlyOutput("incidents")),
+        tabPanel("Service Delivery", plotlyOutput("serv_del")),
+        tabPanel("Elevators/Escalators", plotlyOutput("el_es"))
+      )
+      
     )
-)))
+  )
+))
